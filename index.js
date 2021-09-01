0f0d45931219c23104cf5501113b7209469e90cd
Elías Bobadilla <elias.bobadilla@gmail.com>
2021-09-01 16:21:05 -0500

    Fix linter errors
    Fix linter errors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import 'react-native-gesture-handler'
+/**
+ * @format
+ */
+
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {AppRegristry} from 'react-native'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
